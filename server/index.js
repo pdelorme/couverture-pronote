b26b32d1cea92a2decbbbb5d4d862203d09bc68e
patrice delorme <patrice.delorme@gmail.com>
2024-06-29 20:43:51 +0200

    sauvegarde locale

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express ();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
@@ -6,7 +8,9 @@ app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
   });
 
+var counter = 0;
 app.get("/status", (request, response) => {
+    console.log("GET /status");
     const status = {
        "Status": "Running"
     };
@@ -17,6 +21,16 @@ app.get("/status", (request, response) => {
  app.post('/edtData', function(request, response) {
     // req.file (singular) is the file
     console.log(request.body);
+    counter++;
+    var file_name = "./data/edtData_"+counter+"_"+Date.now()+".json";
+    console.log(file_name);
+    fs.writeFile(file_name, JSON.stringify(request.body), async(err) => {
+        if (err) {
+            console.error(err);
+          } else {
+            console.log("Successfully Written to File.");
+          }
+      });
     const status = {"Status": "OK"};
     response.send(status);
 });
